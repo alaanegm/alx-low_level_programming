9be162dd5ba127216add2c2a8a8b0d159ea4678b
alaanegm <alaanegm@gmail.com>
2023-03-21 16:16:39 -0700

    My 0x02-functions_nested_loops commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,12 +2,11 @@
 
 
 	/**
-	 * print_sign - prints the sign of a number
+	 * print_sign  -  prints the sign of a number
 	 * @n: the int to check
 	 * Return: 1 and prints + if n is greater than zero
 	 * 0 and prints 0 if n is zero
 	 * -1 and prints - if n is less than zero
-	 * function print_sign -  prints the sign of a number
 	 */
 	int print_sign(int n)
 	{
